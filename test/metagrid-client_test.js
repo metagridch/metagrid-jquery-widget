2bee5e228ae8a9eb10dd9cc9380efe631b76aa70
Tmen <t.steiner@captns.ch>
2016-09-07 12:14:53 +0200

    Update thest to new functions

--- test/metagrid-client_test.js
+++ test/metagrid-client_test.js
@@ -12,9 +12,9 @@
     QUnit.asyncTest('async call', 2, function (assert) {
         // expect(2); // we have one async test to run
 
-        var test = setTimeout(function () {
+        setTimeout(function () {
             assert.equal($('#qunit-fixture .blue-box a').attr('href'), 'http://metagrid.ch');
-            assert.equal($('#qunit-fixture .metagrid-link').length, 4);
+            assert.equal($('#qunit-fixture .metagrid-link').length, 5);
             start();
         }, 1000);
     });
@@ -24,10 +24,10 @@
         this.elems.metagridClient({
             projectSlug: 'dds',
             includeDescription: true,
-            template: '<div><span>metagrid</span></div>'
+            template: $('<div><span>metagrid</span><span id="metagrid-links"></span></div>')
         });
 
-        var test =  setTimeout(function () {
+        setTimeout(function () {
             //
             assert.ok($('#qunit-fixture :not(.blue-box)'));
             assert.equal($('#qunit-fixture  span:first').text(), 'metagrid');
@@ -40,12 +40,12 @@
         this.elems.metagridClient({
             projectSlug: 'dds',
             includeDescription: true,
-            template: '<div><span>metagrid</span></div>'
+            template: $('<div><span>metagrid</span><span id="metagrid-links"></span></div>')
         });
         var elem = this.elems;
-        var test = setTimeout(function () {
+        setTimeout(function () {
             //
-            var mock = JSON.parse('{ "HLS": { "url": "http:\/\/www.hls-dhs-dss.ch\/textes\/d\/D4647.php", "short_description": "Historisches Lexikon der Schweiz", "long_description": null }, "GND": { "url": "http:\/\/d-nb.info\/gnd\/124769942", "short_description": "", "long_description": null }, "Elites suisses au XXe s.": { "url": "http:\/\/www2.unil.ch\/elitessuisses\/index.php?page=detailPerso&idIdentite=50020", "short_description": "", "long_description": null }, "Huygens ING": { "url": "http:\/\/resources.huygens.knaw.nl\/europeseintegratie\/en\/persoon\/5148", "short_description": "", "long_description": null } }');
+            var mock = JSON.parse('{"HLS":{"url":"http:\/\/www.hls-dhs-dss.ch\/textes\/d\/D4647.php","short_description":"Historisches Lexikon der Schweiz","long_description":null},"Elites suisses au XXe s.":{"url":"http:\/\/www2.unil.ch\/elitessuisses\/index.php?page=detailPerso&idIdentite=50020","short_description":"","long_description":null},"BSG":{"url":"http:\/\/www.bsg.nb.admin.ch\/search\/query?match_1=PHRASE&field_1=authid&term_1=103283&theme=BSG&locale=de","short_description":"BSG","long_description":"Bibliographie der Schweizergeschichte"},"GND":{"url":"http:\/\/d-nb.info\/gnd\/124769942","short_description":"","long_description":null},"Huygens ING":{"url":"http:\/\/resources.huygens.knaw.nl\/europeseintegratie\/en\/persoon\/5148","short_description":"","long_description":null}    }');
             assert.deepEqual(elem.metagridClient('getData'), mock, "Not the same data in mock and object");
             start();
         }, 1000);
@@ -53,8 +53,8 @@
 
     QUnit.asyncTest('fail to load remote data', 1, function (assert) {
         // expect(2); // we have one async test to run
-        var client = $('#qunit-fixture').children('.failure').metagridClient({projectSlug: 'dds'});
-        var test = setTimeout(function () {
+        $('#qunit-fixture').children('.failure').metagridClient({projectSlug: 'dds'});
+        setTimeout(function () {
             assert.ok($('#qunit-fixture :not(.blue-box)'));
             start();
         }, 5000);
