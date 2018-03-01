fc7f0f68c279c0b206bf9a991a582cbc2f7e4908
Tobias Steiner <tobinski@dodis.ch>
2018-03-01 15:50:51 +0100

    Update tests to qunite2

--- test/metagrid-client_test.js
+++ test/metagrid-client_test.js
@@ -1,27 +1,25 @@
 (function ($) {
-    module('jQuery#metagridClient', {
-        setup: function () {
+    QUnit.module('jQuery#metagridClient', {
+        beforeEach: function () {
             this.elems = $('#qunit-fixture').children('.success').metagridClient({projectSlug: 'dds'});
-
         }
     });
 
-    test('is chainable',1, function () {
-        strictEqual(this.elems.metagridClient({projectSlug: 'dds'}), this.elems, 'should be chainable');
+    QUnit.test('is chainable', function (assert) {
+        assert.strictEqual(this.elems.metagridClient({projectSlug: 'dds'}), this.elems, 'should be chainable');
     });
 
-    QUnit.asyncTest('async call', 2, function (assert) {
-        // expect(2); // we have one async test to run
-
+    QUnit.test('async call', function (assert) {
+        var done = assert.async();
         setTimeout(function () {
             assert.equal($('#qunit-fixture .blue-box a').attr('href'), 'http://metagrid.ch');
-            assert.equal($('#qunit-fixture .metagrid-link').length, 5);
-            start();
+            assert.equal($('#qunit-fixture .metagrid-link').length, 6);
+            done();
         }, 1000);
     });
 
-    QUnit.asyncTest('new template', 2, function (assert) {
-        // expect(2); // we have one async test to run
+    QUnit.test('new template',  function (assert) {
+        var done = assert.async();
         this.elems.metagridClient({
             projectSlug: 'dds',
             includeDescription: true,
@@ -32,24 +30,24 @@
             //
             assert.ok($('#qunit-fixture :not(.blue-box)'));
             assert.equal($('#qunit-fixture  span:first').text(), 'metagrid');
-            start();
+            done();
         }, 1000);
 
     });
 
-    QUnit.asyncTest('load indirectly', 2, function (assert) {
+    QUnit.test('load indirectly',  function (assert) {
+        var done = assert.async();
         var bag =   $('<div/>');
         bag.data({'element-kind': 'person', 'element-id':5}).metagridClient({projectSlug: 'dds'});
         setTimeout(function () {
-            console.log(bag.html());
             assert.ok(bag.find('#metagrid-links').length);
-            assert.equal(bag.find('.metagrid-link').length, 5);
-            start();
+            assert.equal(bag.find('.metagrid-link').length, 6);
+            done();
         }, 1000);
 
     });
-    QUnit.asyncTest('remote data', 1, function (assert) {
-        // expect(2); // we have one async test to run
+    QUnit.test('remote data',  function (assert) {
+        var done = assert.async();
         this.elems.metagridClient({
             projectSlug: 'dds',
             includeDescription: true,
@@ -58,18 +56,49 @@
         var elem = this.elems;
         setTimeout(function () {
             //
-            var mock = JSON.parse('{"HLS":{"url":"http:\/\/www.hls-dhs-dss.ch\/textes\/d\/D4647.php","short_description":"Historisches Lexikon der Schweiz","long_description":null},"Elites suisses au XXe s.":{"url":"http:\/\/www2.unil.ch\/elitessuisses\/index.php?page=detailPerso&idIdentite=50020","short_description":"","long_description":null},"BSG":{"url":"http:\/\/www.bsg.nb.admin.ch\/search\/query?match_1=PHRASE&field_1=authid&term_1=103283&theme=BSG&locale=de","short_description":"BSG","long_description":"Bibliographie der Schweizergeschichte"},"GND":{"url":"http:\/\/d-nb.info\/gnd\/124769942","short_description":"","long_description":null},"Huygens ING":{"url":"http:\/\/resources.huygens.knaw.nl\/europeseintegratie\/en\/persoon\/5148","short_description":"","long_description":null}    }');
+            var mock = JSON.parse('{\n' +
+                '        "HLS": {\n' +
+                '            "url": "http:\\/\\/www.hls-dhs-dss.ch\\/textes\\/d\\/D4647.php",\n' +
+                '            "short_description": "Historisches Lexikon der Schweiz",\n' +
+                '            "long_description": "Historisches Lexikon der Schweiz"\n' +
+                '        },\n' +
+                '        "GND": {\n' +
+                '            "url": "http:\\/\\/d-nb.info\\/gnd\\/124769942",\n' +
+                '            "short_description": "Gemeinsame Normdatei (GND)",\n' +
+                '            "long_description": "Gemeinsame Normdatei (GND)"\n' +
+                '        },\n' +
+                '        "Elites suisses": {\n' +
+                '            "url": "http:\\/\\/www2.unil.ch\\/elitessuisses\\/index.php?page=detailPerso&idIdentite=50020",\n' +
+                '            "short_description": "Schweizerische Eliten im 20. Jahrhundert",\n' +
+                '            "long_description": "Schweizerische Eliten im 20. Jahrhundert"\n' +
+                '        },\n' +
+                '        "Helveticat": {\n' +
+                '            "url": "http:\\/\\/www.helveticat.ch\\/search\\/query?theme=Helveticat&match_1=PHRASE&field_1=authid&term_1=4605046",\n' +
+                '            "short_description": "Helveticat",\n' +
+                '            "long_description": "Helveticat"\n' +
+                '        },\n' +
+                '        "Huygens": {\n' +
+                '            "url": "http:\\/\\/resources.huygens.knaw.nl\\/europeseintegratie\\/en\\/persoon\\/5148",\n' +
+                '            "short_description": "Huygens ING",\n' +
+                '            "long_description": "Huygens ING"\n' +
+                '        },\n' +
+                '        "BSG": {\n' +
+                '            "url": "http:\\/\\/www.bsg.nb.admin.ch\\/search\\/query?match_1=PHRASE&field_1=authid&term_1=103283&theme=BSG&locale=de",\n' +
+                '            "short_description": "BSG",\n' +
+                '            "long_description": "Bibliographie zur Schweizergeschichte"\n' +
+                '        }\n' +
+                '    }');
             assert.deepEqual(elem.metagridClient('getData'), mock, "Not the same data in mock and object");
-            start();
+            done();
         }, 1000);
     });
 
-    QUnit.asyncTest('fail to load remote data', 1, function (assert) {
-        // expect(2); // we have one async test to run
+    QUnit.test('fail to load remote data', function (assert) {
+        var done = assert.async();
         $('#qunit-fixture').children('.failure').metagridClient({projectSlug: 'dds'});
         setTimeout(function () {
             assert.ok($('#qunit-fixture :not(.blue-box)'));
-            start();
+            done();
         }, 5000);
     });
 
